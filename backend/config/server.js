6846296cf5eb44b1699a5b3ea505f53944270b56
Salman Asif <salmanasif@Salmans-MBP.Dlink>
2021-05-22 11:16:17 +0600

    first commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e581249d40b8797319490ab32ff3922c'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ebca7e72-0b42-4ca5-add9-b2a6e86f29a0'
+};
